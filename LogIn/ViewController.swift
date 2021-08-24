66a177f868ef84024895da55af515f32327d89d9
Kirill Batmanov <BatmanovKS@icloud.com>
2021-08-24 19:26:39 +0200

    Initial Commit

---
+++ LogIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogIn
+//
+//  Created by Кирилл Батманов on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
